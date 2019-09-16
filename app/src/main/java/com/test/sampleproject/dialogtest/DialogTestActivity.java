4b3e9a0f1db4344f4981a48b104b84b952371b6e
toctocbin <haanbin@naver.com>
2019-09-16 09:00:56 +0900

    add dialog test

---
+++ app/src/main/java/com/test/sampleproject/dialogtest/DialogTestActivity.java
@@ -0,0 +1,16 @@
+package com.test.sampleproject.dialogtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.test.sampleproject.R;
+
+public class DialogTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_test_acitivity);
+    }
+}
